dc4e563e65af1cdc96bb9405f4e406b733b6435d
JnKDog <530661401@qq.com>
2023-12-15 08:35:07 +0800

    post代码计算

---
+++ 计算机网络/测试代码/tcp-http.py
@@ -0,0 +1,46 @@
+import socket
+import json
+
+def send_json_post_request(host, port, path, headers, json_data):
+    # 将 JSON 数据转换为字符串
+    data_str = json.dumps(json_data)
+
+    # 构建 HTTP 请求
+    request_header = f"POST {path} HTTP/1.1\r\n" \
+              f"Host: {host}\r\n" \
+              f"{headers}\r\n" \
+              "Content-Type: application/json\r\n" \
+              f"Content-Length: {len(data_str)}\r\n" \
+              "\r\n" \
+
+    request_body = f"{data_str}"
+
+    # 创建 TCP 套接字
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # 连接到服务器
+        s.connect((host, port))
+
+        # 发送 HTTP 请求
+        s.sendall(request_header.encode())
+        s.sendall(request_body.encode())
+
+        # 接收服务器响应
+        response = s.recv(1024)
+
+    print("Server response:")
+    print(response.decode())
+
+if __name__ == "__main__":
+    # 设置服务器的主机、端口和路径
+    server_host = "localhost"
+    server_port = 80
+    server_path = "/post"
+
+    # 设置 JSON 格式的数据
+    json_data = {"key1": "value1", "key2": "value2"}
+
+    # 设置自定义的请求头
+    custom_headers = "Authorization: Bearer Token123"
+
+    # 发送 JSON 格式的 POST 请求
+    send_json_post_request(server_host, server_port, server_path, custom_headers, json_data)
